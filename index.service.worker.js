8c43e436eb470fd983dba351b92daac55a78d72d
Masonzk <76796543+Masonzk@users.noreply.github.com>
2023-03-15 23:38:36 -0400

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675996462|7032207312";
+const CACHE_VERSION = "1678937863|6253066628";
 const CACHE_PREFIX = "TSA2023-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
